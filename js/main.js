8cc97065a4acfd7bbb57588b53b5902bd1fb7235
Anthony <79248913+ticket1201@users.noreply.github.com>
2021-06-22 22:26:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Menu click action
+
+var menuEl = document.querySelector('.menu__items');
+var btnEl = document.querySelector('.menu__wrapper');
+
+function toggleMenu() {
+    menuEl.classList.toggle('open');
+}
+
+btnEl.addEventListener('click', toggleMenu);
+
+// Scroll to top btn action
+
+const select = document.querySelector("main");
+const toTopBtn = document.querySelector(".scrollBtn");
+
+const objOptions = {
+  root: null,
+  rootMargin: "-5px",
+};
+
+const sectionObserver = new IntersectionObserver(callBackFunction, objOptions);
+sectionObserver.observe(select);
+
+function callBackFunction(entries) {
+  const [entry] = entries;
+  //console.log(entry);
+  if (entry.isIntersecting) {
+    toTopBtn.classList.remove("block");
+  } else {
+    toTopBtn.classList.add("block");
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
+}
+
+toTopBtn.addEventListener('click', topFunction);
